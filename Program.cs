ed459dd6b2389118fe2aaf3dee6b5966596b03c5
murillojoaquim <murillojalves@hotmail.com>
2021-09-17 18:59:07 -0300

    Calculo Farenheint para Celsius

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Digite o valor em graus Farenheint:");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("°F = ");
+            string Farenheint = Console.ReadLine();
+
+            double Celsius = (Convert.ToDouble(Farenheint) - 32) / 1.8;
+            
+            Console.Write($"\n{Farenheint}°F equivalem a ");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"{Celsius}°C");
+
+            Console.ReadKey();
+            Console.ResetColor();
+        }
+    }
+}
